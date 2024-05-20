5138384722d84c9cb113465e1a9da1b405a4f813
Marco Antonio Gil <gildo_marco@hotmail.com>
2024-05-20 12:23:25 -0300

    Completando NavBar para Mobile

--- src/components/BarraNavegacao.tsx
+++ src/components/BarraNavegacao.tsx
@@ -1,36 +1,27 @@
 import MainBg from './MainBg';
 import NavbarList from './NavbarList';
-import kart from '../assets/kart.png'
+
+
+import NavbarListMobile from './NavbarListMobile';
+import { useIsMobileContext } from '../contexts/useIsMobileContext';
 
 const BarraNavegacao = () => {
+    const isMobile = useIsMobileContext()
 
     return (
 
-        <div
-            className={`h-20 bg-sky-700 flex justify-center`}
+        <header
+            className={`sm:h-20 h-14 bg-sky-700 flex justify-center shadow-custom-bottom relative`}
         >
             <MainBg >
-                <div className=' px-4 w-screen flex items-center justify-stretch text-white text-base font-semibold text-center font-roboto-condensed'>
-                    <NavbarList />
+                <div className=' px-4 w-screen flex items-center justify-between text-white text-lg leading-none text-center font-league-gothic'>
+                    
+                    {isMobile ? <NavbarListMobile /> : <NavbarList />}
 
-                    <div className='w-44 mx-3 '>
-                        <a href="">
-                            <span>
-                                <h6>FAZER LOGIN OU INICIAR CADASTRO</h6>
-                            </span>
-                        </a>
-                    </div>
 
-                    <div className="w-20 ">
-                        <div className='overflow-hidden h-10 relative'>
-                            <span className='bg-red-600 h-5 w-5 absolute z-10 rounded-full right-3 text-sm'>0</span>
-                            <img src={kart} alt="kart" className='scale-x-[-1]' />
-                        </div>
-                        <h6>CARRINHO</h6>
-                    </div>
                 </div>
             </MainBg>
-        </div>
+        </header>
 
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
         extend: {
           fontFamily: {
             'roboto-condensed': ['"Roboto Condensed"', 'sans-serif'],
+            'pathway-gothic': ['"Pathway Gothic One"', 'sans-serif'],
+            'league-gothic': ['"League Gothic"', 'sans-serif']
+          },
+          boxShadow: {
+            'custom-bottom': '0 7px 10px 0 rgba(0, 0, 0, 0.25)'
           },
         },
       },
